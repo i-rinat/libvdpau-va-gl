489fdd8eb9f0726115942d045a32ca17042dbefb
Rinat <ibragimovrinat@mail.ru>
2013-01-09 19:48:00 +0400

    handles for VdpOutputSurfaceData and VdpVideoSurfaceData

--- handle-storage.h
+++ handle-storage.h
@@ -11,6 +11,8 @@ typedef uint32_t    HandleType;
 #define HANDLE_TYPE_PRESENTATION_QUEUE_TARGET   (HandleType)2
 #define HANDLE_TYPE_PRESENTATION_QUEUE          (HandleType)3
 #define HANDLE_TYPE_VIDEO_MIXER                 (HandleType)4
+#define HANDLE_TYPE_OUTPUT_SURFACE              (HandleType)5
+#define HANDLE_TYPE_VIDEO_SURFACE               (HandleType)6
 
 typedef struct {
     HandleType type;
@@ -39,6 +41,25 @@ typedef struct {
     VdpDevice device;
 } VdpVideoMixerData;
 
+typedef struct {
+    HandleType type;
+    VdpDevice device;
+    VdpRGBAFormat rgba_format;
+    uint32_t width;
+    uint32_t stride;
+    uint32_t height;
+    void *buf;
+} VdpOutputSurfaceData;
+
+typedef struct {
+    HandleType type;
+    VdpDevice device;
+    VdpChromaType chroma_type;
+    uint32_t width;
+    uint32_t stride;
+    uint32_t height;
+    void *buf;
+} VdpVideoSurfaceData;
 
 void handlestorage_initialize(void);
 int handlestorage_add(void *data);
