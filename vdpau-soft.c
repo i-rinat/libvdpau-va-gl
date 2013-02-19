6fa3c60e11e0bdbeb32ad6306e3492f006656b2f
Rinat <ibragimovrinat@mail.ru>
2013-02-19 16:56:10 +0400

    set texture parameters of bitmap surface just after creation

--- vdpau-soft.c
+++ vdpau-soft.c
@@ -979,6 +979,11 @@ softVdpBitmapSurfaceCreate(VdpDevice device, VdpRGBAFormat rgba_format, uint32_t
 
     glXMakeCurrent(deviceData->display, deviceData->root, deviceData->glc);
     glGenTextures(1, &data->tex_id);
+    glBindTexture(GL_TEXTURE_2D, data->tex_id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_BGRA, GL_UNSIGNED_BYTE, NULL);
 
     *surface = handlestorage_add(data);
