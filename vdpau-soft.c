d6aff8aa5f9e3fb196f2ead64ff133c66ce02bed
Rinat <ibragimovrinat@mail.ru>
2013-03-06 20:41:22 +0400

    further parsing of slice_header

--- vdpau-soft.c
+++ vdpau-soft.c
@@ -563,6 +563,98 @@ softVdpDecoderRender(VdpDecoder decoder, VdpVideoSurface target,
                         }
                     }
                 }
+                if (20 == nal_unit_type) {
+                    assert(0 && "not implemented");
+                } else {
+                    // begin of ref_pic_list_modification( )
+                    if (2 != slice_type && 4 != slice_type) {
+                        int ref_pic_list_modification_flag_l0 = rbsp_get_u(&st, 1);
+                        fprintf(stderr, "ref_pic_list_modification_flag_l0 = %d\n",
+                            ref_pic_list_modification_flag_l0);
+                        if (ref_pic_list_modification_flag_l0) {
+                            int modification_of_pic_nums_idc;
+                            do {
+                                modification_of_pic_nums_idc = rbsp_get_uev(&st);
+                                if (0 == modification_of_pic_nums_idc ||
+                                    1 == modification_of_pic_nums_idc)
+                                {
+                                    fprintf(stderr, "abs_diff_pic_num_minus1 = %d\n", rbsp_get_uev(&st));
+                                } else if (2 == modification_of_pic_nums_idc) {
+                                    fprintf(stderr, "long_term_pic_num = %d\n", rbsp_get_uev(&st));
+                                }
+                            } while (modification_of_pic_nums_idc != 3);
+
+                        }
+                    }
+
+                    if (1 == slice_type) {
+                        int ref_pic_list_modification_flag_l1 = rbsp_get_u(&st, 1);
+                        if (ref_pic_list_modification_flag_l1) {
+                            int modification_of_pic_nums_idc;
+                            do {
+                                modification_of_pic_nums_idc = rbsp_get_uev(&st);
+                                if (0 == modification_of_pic_nums_idc ||
+                                    1 == modification_of_pic_nums_idc)
+                                {
+                                    fprintf(stderr, "abs_diff_pic_num_minus1 = %d\n", rbsp_get_uev(&st));
+                                } else if (2 == modification_of_pic_nums_idc) {
+                                    fprintf(stderr, "long_term_pic_num = %d\n", rbsp_get_uev(&st));
+                                }
+                            } while (modification_of_pic_nums_idc != 3);
+                        }
+                    }
+                    // end of ref_pic_list_modification( )
+                }
+
+                if ((vdppi->weighted_pred_flag &&
+                    (SLICE_TYPE_P == slice_type || SLICE_TYPE_SP == slice_type)) ||
+                    (1 == vdppi->weighted_bipred_idc && SLICE_TYPE_B == slice_type))
+                {
+                    // begin of pred_weight_table( )
+                    fprintf(stderr, "luma_log2_weight_denom = %d\n", rbsp_get_uev(&st));
+                    if (0 != ChromaArrayType) {
+                        fprintf(stderr, "chroma_log2_weight_denom = %d\n", rbsp_get_uev(&st));
+                    }
+                    for (int k = 0; k <= vdppi->num_ref_idx_l0_active_minus1; k ++) {
+                        int luma_weight_l0_flag = rbsp_get_u(&st, 1);
+                        fprintf(stderr, "luma_weight_l0_flag = %d\n", luma_weight_l0_flag);
+                        if (luma_weight_l0_flag) {
+                            fprintf(stderr, "luma_weight_l0[%d] = %d\n", k, rbsp_get_sev(&st));
+                            fprintf(stderr, "luma_offset_l0[%d] = %d\n", k, rbsp_get_sev(&st));
+                        }
+                        if (0 != ChromaArrayType) {
+                            int chroma_weight_l0_flag = rbsp_get_u(&st, 1);
+                            fprintf(stderr, "chroma_weight_l0_flag = %d\n", chroma_weight_l0_flag);
+                            if (chroma_weight_l0_flag) {
+                                for (int j = 0; j < 2; j ++) {
+                                    fprintf(stderr, "chroma_weight_l0[%d][%d] = %d\n", k, j, rbsp_get_sev(&st));
+                                    fprintf(stderr, "chroma_offset_l0[%d][%d] = %d\n", k, j, rbsp_get_sev(&st));
+                                }
+                            }
+                        }
+                    }
+                    if (1 == slice_type) {
+                        for (int k = 0; k <= vdppi->num_ref_idx_l1_active_minus1; k ++) {
+                            int luma_weight_l1_flag = rbsp_get_u(&st, 1);
+                            fprintf(stderr, "luma_weight_l1_flag = %d\n", luma_weight_l1_flag);
+                            if (luma_weight_l1_flag) {
+                                fprintf(stderr, "luma_weight_l1[%d] = %d\n", k, rbsp_get_sev(&st));
+                                fprintf(stderr, "luma_offset_l1[%d] = %d\n", k, rbsp_get_sev(&st));
+                            }
+                            if (0 != ChromaArrayType) {
+                                int chroma_weight_l1_flag = rbsp_get_u(&st, 1);
+                                fprintf(stderr, "chroma_weight_l1_flag = %d\n", chroma_weight_l1_flag);
+                                if (chroma_weight_l1_flag) {
+                                    for (int j = 0; j < 2; j ++) {
+                                        fprintf(stderr, "chroma_weight_l1[%d][%d] = %d\n", k, j, rbsp_get_sev(&st));
+                                        fprintf(stderr, "chroma_offset_l1[%d][%d] = %d\n", k, j, rbsp_get_sev(&st));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    // end of pred_weight_table( )
+                }
 
 
 
