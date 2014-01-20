d215695d56d611df859a728cc7c68dc131688171
Rinat <ibragimovrinat@mail.ru>
2014-01-21 01:15:35 +0400

    define own set of AV_PIX_FMT_* for libavutils older than (51,42,0)

---
+++ src/pixfmt_compat.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2013  Rinat Ibragimov
+ *
+ * This file is part of libvdpau-va-gl
+ *
+ * libvdpau-va-gl is distributed under the terms of the LGPLv3. See COPYING for details.
+ */
+
+#ifndef VA_GL_SRC_PIXFMT_COMPAT_H
+#define VA_GL_SRC_PIXFMT_COMPAT_H
+
+#include <libavutil/pixfmt.h>
+
+#if LIBAVUTIL_VERSION_INT < AV_VERSION_INT(51, 42, 0)
+
+#define AV_PIX_FMT_NONE         PIX_FMT_NONE
+#define AV_PIX_FMT_NV12         PIX_FMT_NV12
+#define AV_PIX_FMT_YUV420P      PIX_FMT_YUV420P
+#define AV_PIX_FMT_UYVY422      PIX_FMT_UYVY422
+#define AV_PIX_FMT_YUYV422      PIX_FMT_YUYV422
+
+#endif // old libavutil version
+
+#endif // VA_GL_SRC_PIXFMT_COMPAT_H
