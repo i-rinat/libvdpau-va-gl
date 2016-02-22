9d2a3fcd409964953fc055ef47fa760a2da32ce7
Rinat Ibragimov <ibragimovrinat@mail.ru>
2016-02-22 13:05:02 +0300

    h264-parse: move defines inside .c file

--- src/h264-parse.c
+++ src/h264-parse.c
@@ -13,6 +13,19 @@
 #include <glib.h>
 #include "h264-parse.h"
 
+#define SLICE_TYPE_P    0
+#define SLICE_TYPE_B    1
+#define SLICE_TYPE_I    2
+#define SLICE_TYPE_SP   3
+#define SLICE_TYPE_SI   4
+
+#define NAL_UNSPECIFIED     0
+#define NAL_SLICE           1
+#define NAL_SLICE_DATA_A    2
+#define NAL_SLICE_DATA_B    3
+#define NAL_SLICE_DATA_C    4
+#define NAL_IDR_SLICE       5
+
 #define NOT_IMPLEMENTED(str)        assert(0 && "not implemented" && str)
 
 #define DESCRIBE(xparam, format)    fprintf(stderr, #xparam " = %" #format "\n", xparam)
--- src/h264-parse.h
+++ src/h264-parse.h
@@ -12,18 +12,6 @@
 #include <va/va.h>
 #include "bitstream.h"
 
-#define SLICE_TYPE_P    0
-#define SLICE_TYPE_B    1
-#define SLICE_TYPE_I    2
-#define SLICE_TYPE_SP   3
-#define SLICE_TYPE_SI   4
-
-#define NAL_UNSPECIFIED     0
-#define NAL_SLICE           1
-#define NAL_SLICE_DATA_A    2
-#define NAL_SLICE_DATA_B    3
-#define NAL_SLICE_DATA_C    4
-#define NAL_IDR_SLICE       5
 
 void
 parse_slice_header(rbsp_state_t *st, const VAPictureParameterBufferH264 *vapp,
