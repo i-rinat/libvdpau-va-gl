ce629254596c9fde6b9af6f7d227febc35b9659e
Rinat Ibragimov <ibragimovrinat@mail.ru>
2016-02-23 20:35:06 +0300

    h264-parser: replace macros by enums for constants

--- src/h264-parse.c
+++ src/h264-parse.c
@@ -13,18 +13,22 @@
 #include <glib.h>
 #include "h264-parse.h"
 
-#define SLICE_TYPE_P    0
-#define SLICE_TYPE_B    1
-#define SLICE_TYPE_I    2
-#define SLICE_TYPE_SP   3
-#define SLICE_TYPE_SI   4
+enum {
+    SLICE_TYPE_P =  0,
+    SLICE_TYPE_B =  1,
+    SLICE_TYPE_I =  2,
+    SLICE_TYPE_SP = 3,
+    SLICE_TYPE_SI = 4,
+};
 
-#define NAL_UNSPECIFIED     0
-#define NAL_SLICE           1
-#define NAL_SLICE_DATA_A    2
-#define NAL_SLICE_DATA_B    3
-#define NAL_SLICE_DATA_C    4
-#define NAL_IDR_SLICE       5
+enum {
+    NAL_UNSPECIFIED  =  0,
+    NAL_SLICE        =  1,
+    NAL_SLICE_DATA_A =  2,
+    NAL_SLICE_DATA_B =  3,
+    NAL_SLICE_DATA_C =  4,
+    NAL_IDR_SLICE    =  5,
+};
 
 #define NOT_IMPLEMENTED(str)        assert(0 && "not implemented" && str)
 
